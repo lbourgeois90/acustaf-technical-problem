ed73ed32bd3d2058cd3ba85dd64a49e86cca6f4c
Lili Bourgeois <lbourgeois90@gmail.com>
2019-06-14 18:30:56 -0500

    create get request to api endpoint

--- server/routes/jsonplaceholder.router.js
+++ server/routes/jsonplaceholder.router.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+
+router.get ('/', (req,res) =>{
+    console.log('in GET request');
+    axios(`https://jsonplaceholder.typicode.com/posts`)
+    .then ( (result) => {
+        res.send(result.data)
+    })
+    .catch( (error) => {
+        console.log('ERROR in GET');
+        res.sendStatus(500);
+    })
+})
 
 
 module.exports = router;
