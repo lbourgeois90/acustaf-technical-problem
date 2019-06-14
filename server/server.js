9e22bc8eac13c573cb28ff062bbe824084ef4d59
Lili Bourgeois <lbourgeois90@gmail.com>
2019-06-14 18:24:02 -0500

    import jsonplaceholder.router into server.js, export app for testing

--- server/server.js
+++ server/server.js
@@ -2,16 +2,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const jsonplaceholder = require('../server/routes/jsonplaceholder.router');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
-
+app.use('/api/jsonplaceholder', jsonplaceholder);
 /** ---------- ROUTES ---------- **/
+
 
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
     console.log('Listening on port: ', PORT);
 });
+
+module.exports = app;
