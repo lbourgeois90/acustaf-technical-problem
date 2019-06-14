2813aff902191003f26a779d5c542f86c42a775b
Lili Bourgeois <lbourgeois90@gmail.com>
2019-06-14 18:52:55 -0500

    testing get route to api endpoint passes jest test

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -2,8 +2,8 @@ let app = require('../server')
 let testServer = require('supertest')
 
 describe('Test API endpoint GET request', () => {
-    test('Should respond with status 200'), async () => {
+    test('Should respond with status 200', async () => {
         const response = await testServer(app).get('/api/jsonplaceholder');
         expect(response.statusCode).toBe(200);
-    }
+      });
 })
