a84950a462ce1e38dea863aa02f87d42b7dff0f4
Lili Bourgeois <lbourgeois90@gmail.com>
2019-06-14 21:06:40 -0500

    comment out text field and try buttons?

--- src/App.js
+++ src/App.js
@@ -55,34 +55,55 @@ class App extends Component {
         alignItems='center'>
 
           <Grid item align='center'>
-            <Typography variant='h4' className={classes.headerTitle}>Acustaf Front-End Developer Technical Problem</Typography>
+            <img src='/images/logo.png' align='center' className={classes.headerImage}></img>
+            <Typography variant='h4' className={classes.headerTitle}>Front-End Developer Technical Problem</Typography>
             <Divider className={classes.headerDivider}/>
           </Grid>
 
-          <Grid item>
-            <TextField
-              helperText='Choose a number between 1-100'
+          <Grid item className={classes.selectionGrid}>
+            <Typography variant='h4' className={classes.headerTitle} align='center'>Please Choose A Number Between 1-20</Typography>
+            {/* <TextField
               margin='normal'
               type='number'
               InputProps={{ inputProps: { min: 1, max: 100 } }}
               value={this.state.selectedNumber}
               onChange={this.handleChange('selectedNumber')}
               className={classes.textField}
-            />
-            <Button variant="outlined" onClick={this.handleSubmit} className={classes.button}>
-              Submit
-            </Button>
+            /> */}
+          </Grid>
+
+          <Grid item>
+            <Button onClick={this.handleChange(1)}>01</Button>
+            <Button onClick={this.handleChange(2)}>02</Button>
+            <Button onClick={this.handleChange(3)}>03</Button>
+            <Button onClick={this.handleChange(4)}>04</Button>
+            <Button onClick={this.handleChange(5)}>05</Button>
+            <Button onClick={this.handleChange(6)}>06</Button>
+            <Button onClick={this.handleChange(7)}>07</Button>
+            <Button onClick={this.handleChange(8)}>08</Button>
+            <Button onClick={this.handleChange(9)}>09</Button>
+            <Button onClick={this.handleChange(10)}>10</Button>
+            <Button onClick={this.handleChange(11)}>11</Button>
+            <Button onClick={this.handleChange(12)}>12</Button>
+            <Button onClick={this.handleChange(13)}>13</Button>
+            <Button onClick={this.handleChange(14)}>14</Button>
+            <Button onClick={this.handleChange(15)}>15</Button>
+            <Button onClick={this.handleChange(16)}>16</Button>
+            <Button onClick={this.handleChange(17)}>17</Button>
+            <Button onClick={this.handleChange(18)}>18</Button>
+            <Button onClick={this.handleChange(19)}>19</Button>
+            <Button onClick={this.handleChange(20)}>20</Button>
           </Grid>
 
           <Grid item>
             {this.props.reduxState.JSONPLACEHOLDERReducer.id ?
               <Card>
-                <CardContent>
-                  <Typography variant='h6'>Title</Typography>
-                  <Typography variant='h5'>{this.props.reduxState.JSONPLACEHOLDERReducer.title}</Typography>
+                <CardContent className={classes.cardContent}>
+                  <Typography variant='h6' className={classes.cardTypographyHeader}>Title</Typography>
+                  <Typography variant='h5' className={classes.cardTypography}>{this.props.reduxState.JSONPLACEHOLDERReducer.title}</Typography>
                   <Divider/>
-                  <Typography variant='h6'>Body</Typography>
-                  <Typography variant='h5'>{this.props.reduxState.JSONPLACEHOLDERReducer.body}</Typography>
+                  <Typography variant='h6' className={classes.cardTypographyHeader}>Body</Typography>
+                  <Typography variant='h5' className={classes.cardTypography}>{this.props.reduxState.JSONPLACEHOLDERReducer.body}</Typography>
                 </CardContent>
               </Card>
               :
@@ -106,7 +127,13 @@ class App extends Component {
 
 const styles = theme => ({
   headerTitle:{
-    fontFamily: 'Merriweather', 
+    fontFamily: 'Montserrat', 
+    color: 'black',
+    height: '10vh',
+    lineHeight: '10vh',
+  },
+  headerImage:{
+    marginTop: 100,
   },
   headerDivider:{
     width: '100vw'
@@ -114,6 +141,12 @@ const styles = theme => ({
   button: {
     display: 'block',
     margin: '0 auto',
+  },
+  cardTypography:{
+    fontFamily: 'Montserrat', 
+  },
+  cardTypographyHeader:{
+    fontFamily: 'Montserrat', 
   },
 
 
