4b6d4322d37403b6e312736e197d47007dfeac0b
Lili Bourgeois <lbourgeois90@gmail.com>
2019-06-15 12:13:42 -0500

    workthrough checkbox logic to do put update

--- src/todolistitem.js
+++ src/todolistitem.js
@@ -2,27 +2,59 @@ import React, {Component, Fragment} from 'react';
 import {connect} from 'react-redux';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Checkbox from '@material-ui/core/Checkbox';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class ToDoListItem extends Component {
 
+    state={
+        completed: '',
+        checked:false,
+    }
+
+    componentDidMount(){
+        this.setState({
+            completed: this.props.item.completed,
+            checked: this.props.item.completed,
+            id: this.props.item.id,
+            title: this.props.item.title,
+        })
+    }
+
+    handleCheck = (event) => {
+        console.log('in handleCheck')
+        this.setState({
+            ...this.state,
+            checked: !this.state.checked,
+            completed:  !this.state.checked
+        })
+    }
 
 
   render(){
-    // console.log('State is:', this.state);
+    console.log('Item is:', this.props.item);
     const {classes} = this.props;
+    console.log('state is', this.state)
 
-    let iconToDisplay = null;
+    let checkboxToDisplay = null;
     if(this.props.item.completed === true){
-        iconToDisplay = <FontAwesomeIcon icon="check-square"/>
+        checkboxToDisplay =  
+            <Checkbox 
+                onChange={this.handleCheck} 
+                value={this.state.completed}
+                checked={this.state.checked}
+            />
     }
     else if (this.props.item.completed === false){
-        iconToDisplay = <FontAwesomeIcon icon="square"/>
+        checkboxToDisplay = 
+            <Checkbox 
+                onChange={this.handleCheck} 
+                value={this.state.completed}
+                checked={this.state.checked}
+            />
     }
 
     return (
@@ -33,7 +65,7 @@ class ToDoListItem extends Component {
                 </ListItemIcon>
                 <ListItemText primary={this.props.item.title}/>
                 <ListItemIcon>
-                    {iconToDisplay}
+                    {checkboxToDisplay}
                 </ListItemIcon>
               </ListItem>
       </Grid>
