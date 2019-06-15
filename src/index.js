5a2018e717d0668b687274dd296d218fdbed8a70
Lili Bourgeois <lbourgeois90@gmail.com>
2019-06-14 19:40:48 -0500

    revise index.js to work with redux/saga

--- src/index.js
+++ src/index.js
@@ -2,11 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+// Provider allows us to use redux within our react app
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import axios from 'axios';
+// Import saga middleware
+import createSagaMiddleware from 'redux-saga';
+import {takeLatest, put} from 'redux-saga/effects';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//watcher saga to take in dispatches
+function* watcherSaga() {
+}
+
+
+// Create sagaMiddleware
+const sagaMiddleware = createSagaMiddleware();
+
+
+// Create one store that all components can use
+const storeInstance = createStore(
+    combineReducers({
+    }),
+    // Add sagaMiddleware to store
+    applyMiddleware(sagaMiddleware, logger),
+);
+
+// Pass rootSaga into sagaMiddleware
+sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(<Provider store={storeInstance}><App/></Provider>, 
+    document.getElementById('root'));
