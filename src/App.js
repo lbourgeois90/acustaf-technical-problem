d3fba36b312f9fe985451d6576bd0bcffeb3e5df
Lili Bourgeois <lbourgeois90@gmail.com>
2019-06-14 19:35:06 -0500

    change app.js from function to class component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
-function App() {
-  return (
-    <Grid spacing={24}
-      container
-      direction='column'
-      justify='center'
-      alignItems='center'>
+class App extends Component {
 
-        <Grid item align='center'>
-          <Typography variant='h4'>Acustaf Front-End Developer Technical Problem</Typography>
-        </Grid>
+  render(){
+    return (
+      <Grid spacing={24}
+        container
+        direction='column'
+        justify='center'
+        alignItems='center'>
 
-        <Grid item>
-          <TextField
-            helperText='Choose a number between 1-100'
-            margin='normal'
-            type='number'
-            InputProps={{ inputProps: { min: 1, max: 100 } }}
-          />
-          <Button variant="outlined">
-            Submit
-          </Button>
-          
-        </Grid>
-      
+          <Grid item align='center'>
+            <Typography variant='h4'>Acustaf Front-End Developer Technical Problem</Typography>
+          </Grid>
 
-
-
-
+          <Grid item>
+            <TextField
+              helperText='Choose a number between 1-100'
+              margin='normal'
+              type='number'
+              InputProps={{ inputProps: { min: 1, max: 100 } }}
+            />
+            <Button variant="outlined">
+              Submit
+            </Button>
+            
+          </Grid>
+        
 
 
 
 
 
-    </Grid>
-  );
+
+
+
+
+      </Grid>
+    );
+  }
 }
 
 export default App;
